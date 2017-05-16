feb75c97d119f22b0eea4c6e9aae9ec21bd89a91
George Sainsbury <george.sainsbury@adelaide.edu.au>
2017-05-16 16:29:37 +0930

    scale corrections

--- planteye-to-minecraft.py
+++ planteye-to-minecraft.py
@@ -4,18 +4,13 @@ import mcpi.block as block
 import numpy
 import sys
 
-# Minecraft coordinates
-# It goes from -MAX_XZ to MAX_XZ.
-MAX_XZ = 128
-MAX_Y = 64
-
 # Clear board
 def clear_board(minecraft):
     minecraft.setBlocks(-MAX_XZ, 0, -MAX_XZ, MAX_XZ, MAX_Y, MAX_XZ, 0)
 
 # Can be used to work out extent of .ply file points
 def calculate_min_max(plydata):
-	global x_min, x_max, y_min, y_max, z_min, z_max
+	global w_min, w_max, l_min, l_max, h_min, h_max
 
 	w_min = plydata['vertex'][0][0]
 	l_min = plydata['vertex'][0][1]
@@ -48,6 +43,7 @@ def calculate_min_max(plydata):
 
 # Draw plants
 def draw_plant(minecraft, plydata):
+	global w_min, w_max, l_min, l_max, h_min, h_max
 
 	w_range = w_max - w_min
 	l_range = l_max - l_min
@@ -57,12 +53,14 @@ def draw_plant(minecraft, plydata):
 	counts = numpy.zeros((2*MAX_XZ,MAX_Y,2*MAX_XZ))
 
 	# Count points within each minecraft block
+	# X/Z coordinates are offset by MAX_XZ in the positive direction to simplify 
+	# array indexing but this is accounted for in the drawing
 	for v in plydata['vertex']:
 		# planteye has Z for height, minecraft has Y (h).
 		# w -> X, l -> Z, h _> Y
-		mc_X = (((v[0] - w_min) * (2 * MAX_XZ - 1)) / w_range) + (- MAX_XZ) + MAX_XZ
-		mc_Z = (((v[1] - l_min) * (2 * MAX_XZ - 1)) / l_range) + (- MAX_XZ) + MAX_XZ
-		mc_Y = (((v[2] - h_min) * (MAX_Y - 1)) / h_range)
+		mc_X = (((v[0] - w_min) * (2*MAX_XZ - 1)) / w_range)
+		mc_Z = (((v[1] - l_min) * (2*MAX_XZ - 1)) / l_range)
+		mc_Y = (((v[2] - h_min) * (MAX_Y - 1 - 0)) / h_range) + 0
 
 		counts[mc_X][mc_Y][mc_Z]+=1
 
@@ -73,21 +71,41 @@ def draw_plant(minecraft, plydata):
 				#print(i,j,k,counts[i][j][k])
 				if counts[i][j][k] > POINT_COUNT:
 					# Colour top half light green, bottom half dark green
-					mc.setBlock(i-MAX_XZ, MAX_Y - j, k-MAX_XZ, block.WOOL.id if counts[i][j][k] > THICK_POINT_COUNT else block.BEDROCK_INVISIBLE.id, 13 if j > MAX_Y/2 else 5)
+					# Set high density points to WOOL, low denisty to GLASS
+					# Adjust for upside-down in Y and index-shifting in XZ
+					mc.setBlock(i - MAX_XZ, 
+						MAX_Y - j, 
+						k - MAX_XZ, 
+						block.WOOL.id if counts[i][j][k] > THICK_POINT_COUNT else block.BEDROCK_INVISIBLE.id, 
+						13 if j > MAX_Y/2 else 5)
 
 
 
 plydata = PlyData.read(sys.argv[1])
 
 # Manually set planteye coordinate space to ensure no aspect-ratio problems
-#calculate_mix_max(plydata)
-w_min = -750
-w_max = 750
-l_min = 0
-l_max = 1500
-h_min = 600
-h_max = 1100
+calculate_min_max(plydata)
+#h_min = 690
+#h_max = 1100
+#w_min = -205
+#l_min = 0
 
+# assume h is limiting scale.
+scale_factor = h_max - h_min
+print(scale_factor)
+w_min = w_min 
+w_max = w_min + scale_factor*4
+l_min = l_min
+l_max = l_min + scale_factor*4
+print(w_min, w_max)
+print(l_min, l_max)
+print(h_min, h_max)
+
+
+# Minecraft coordinates
+# It goes from -MAX_XZ to MAX_XZ.
+MAX_XZ = 128
+MAX_Y = 64
 # Number of points required in minecraft block to print it.
 POINT_COUNT = 1
 THICK_POINT_COUNT = 3
